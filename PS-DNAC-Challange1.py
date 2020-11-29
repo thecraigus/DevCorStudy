b43f0d0463d41d73732741371c478009485aab01
Craig Cartlidge <clcartlidge@gmail.com>
2020-11-29 18:32:17 +0000

    'ps challange 1'

--- PS-DNAC-Challange1.py
+++ PS-DNAC-Challange1.py
@@ -1,1 +1,33 @@
-#this is a test file
+import requests
+import json
+
+baseurl = "https://sandboxdnac2.cisco.com"
+username = "devnetuser"
+password = "Cisco123!"
+headers = {'content-type': 'application/json'}
+
+
+
+def gettoken(username, password):
+    authreq = requests.post(baseurl+"/dna/system/api/v1/auth/token", auth=(username, password), headers=headers, verify=False)
+    token = authreq.json()
+    return token['Token']
+
+def getdevices(token):
+    devicelist = {}
+    authheaders = {'x-auth-token': token, 'content-type':'application/json' }
+    devreq = requests.get(baseurl+"/dna/intent/api/v1/network-device", headers=authheaders, verify=False).text
+    devices = json.loads(devreq)
+    for device in devices['response']:
+        hostname = device['hostname']
+        ipaddr = device['managementIpAddress']
+        devicelist[hostname] = [ipaddr]
+    return (devicelist)
+
+def main():
+    token = (gettoken(username,password))
+    for device, ipaddr in getdevices(token).items():
+        print (device, ipaddr[0])
+
+if __name__ == '__main__':
+    main()
